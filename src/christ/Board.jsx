a19ba2cbf2ab4f048cf84ad27254f7c9495e72f8
Albert <cool.gilmitdinov@mail.ru>
2022-02-05 20:04:36 +0400

    wrote function to create squares

--- src/christ/Board.jsx
+++ src/christ/Board.jsx
@@ -1,36 +1,24 @@
-import React from 'react';
-import Square from './Square'
+import React from "react";
+import Square from "./Square";
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        activeValue={this.props.activeValue}
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-        className={this.props.activeValue === i+1 ? 'chosen' : ''}
-      />
-    );
-  }
-
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <ul className="board-row">
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((index, i) => {
+            return (
+                <Square
+                  activeValue={this.props.activeValue}
+                  key={i}
+                  index={index}
+                  value={this.props.squares[i]}
+                  onClick={() => this.props.onClick(i)}
+                  className={this.props.activeValue === i + 1 ? "chosen" : ""}
+                />
+            );
+          })}
+        </ul>
       </div>
     );
   }
