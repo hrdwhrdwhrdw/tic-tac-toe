e4abbd91425c57c3e91f4cde6f548ec632c19021
Albert <cool.gilmitdinov@mail.ru>
2022-02-12 19:08:28 +0400

    create-nested-function-to-add-squares

--- src/christ/Board.jsx
+++ src/christ/Board.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import Square from './Square'
+import React from "react";
+import Square from "./Square";
 
 class Board extends React.Component {
+  createSquares() {
+    const field = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    let rowLength = 3;
+    let size = field.length / rowLength;
+    let rows = [];
+    for (let i = 0; i < size; i++) {
+      rows[i] = field.slice(i * size, i * size + size);        // rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    }
+    
+    // return rows.map((key) => {
+    //   return <div className='board-row'>
+    //     {key.map(i => this.renderSquare(i))}
+    //   </div>
+    // });
+
+
+    return rows.map((key) => {
+      console.log(key)
+      return <div className='board-row'>
+        {key.map(i => {return <Square
+        activeValue={this.props.activeValue}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        className={this.props.activeValue ===  i +1 ? "chosen" : ""}
+      />})}
+      </div>
+    });
+    
+  }
+
   renderSquare(i) {
     return (
       <Square
         activeValue={this.props.activeValue}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
-        className={this.props.activeValue === i+1 ? 'chosen' : ''}
+        className={this.props.activeValue ===  i +1 ? "chosen" : ""}
       />
     );
   }
@@ -16,21 +46,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createSquares()}
       </div>
     );
   }
